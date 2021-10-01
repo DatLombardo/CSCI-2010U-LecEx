73057612f26f335796ac60fa17e2b2839fe37dd6
Michael Lombardo <Michael.Lombardo@uoit.net>
2021-10-01 13:36:42 -0400

    Added solutions to lecture examples.
    Additionally, the mistake in Section 009 in the second example was fixed such that n(3(n/2)) instead of 3(n/2)

--- Section-001/W4 - Algorithm Analysis/OperationCounting.java
+++ Section-001/W4 - Algorithm Analysis/OperationCounting.java
@@ -1,33 +1,33 @@
 public class OperationCounting {
     // worst case analysis
-    public static int linearSearch(int[] values, int toFind) { //
-        for (int i = 0; i < values.length; i++) {              //
-           if (values[i] == toFind) {                          //
-               return i;                                       //
+    public static int linearSearch(int[] values, int toFind) { // Assume n = values.length
+        for (int i = 0; i < values.length; i++) {              // 2n + 2 = O(n)
+           if (values[i] == toFind) {                          // 2 = O(1)
+               return i;                                       // 1 = O(1)
            }
         }
-        return -1;                                             //
-     }                                                  // TOTAL: = O()
+        return -1;                                             // 1 = O(1)
+     }                                         // TOTAL: 2n + 2 + 2n + 1 = 4n + 3 = O(n)
 
      public static double[] prefixAverage1(double[] elements) {
-        double[] averages = new double[elements.length];                    //
-        for (int avgIndex = 0; avgIndex < elements.length; avgIndex++) {    //
-            double total = 0.0;                                             //
-            for (int sumIndex = 0; sumIndex <= avgIndex; sumIndex++) {      //
-                total += elements[sumIndex];                                //
+        double[] averages = new double[elements.length];                    // 3 = O(1)
+        for (int avgIndex = 0; avgIndex < elements.length; avgIndex++) {    // 2n + 2 = O(n)
+            double total = 0.0;                                             // 2 = O(1)
+            for (int sumIndex = 0; sumIndex <= avgIndex; sumIndex++) {      // 2(n/2) + 2
+                total += elements[sumIndex];                                // 3 = O(1)
             }
-            averages[avgIndex] = total / (avgIndex + 1);                    //
+            averages[avgIndex] = total / (avgIndex + 1);                    // 4 = O(1)
         }
-        return averages;                                                    //
-    }                                                                 // TOTAL:  = O()
+        return averages;                                                    // 1 = O(1)
+    }                            // TOTAL: 3 + 2n + 2 + 2n + 2n^2/2 + 2n + n(3(n/2)) + 4n + 1 = O(n^2)
 
     public static double[] prefixAverage2(double[] elements) {
-        double[] averages = new double[elements.length];                    //
-        double total = 0.0;                                                 //
-        for (int avgIndex = 0; avgIndex < elements.length; avgIndex++) {    //
-            total += elements[avgIndex];                                    //
-            averages[avgIndex] = total / (avgIndex + 1);                    //
+        double[] averages = new double[elements.length];                    // 4 = O(1)
+        double total = 0.0;                                                 // 2 = O(1)
+        for (int avgIndex = 0; avgIndex < elements.length; avgIndex++) {    // 2 + 2n = O(n)
+            total += elements[avgIndex];                                    // 3 = O(1)
+            averages[avgIndex] = total / (avgIndex + 1);                    // 4 = O(1)
         }
-        return averages;                                                    //
-    }                                                                 // TOTAL:  = O()
+        return averages;                                                    // 1 = O(1)
+    }                                                                // TOTAL: 4 + 2 + 2n + 2 + 3n + 4n = O(n)
 }
